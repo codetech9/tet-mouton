ffe7054a3cecad82d4aba4fc52f88856168b1b26
dylan dol <dylan.dol@orange.fr>
2022-05-03 16:38:35 -0400

    ajout routes lands/bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'lands/index'
-  get 'lands/show'
-  get 'lands/new'
-  get 'lands/create'
-  get 'lands/update'
-  get 'lands/edit'
-  get 'lands/destroy'
+
   devise_for :users
   get 'pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -13,4 +7,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "pages#home"
+  resources :lands do
+    resources :bookings, only: %i[new create]
+  end
 end
